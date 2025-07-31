0463afa1fd6a86df713d8454dd7ad3efe89d8d3a
yamilrafart2 <yamilrafart2>
2025-07-31 19:00:44 -0300

    Declaro array para el almacenamiento de los nombres

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+    Array, que almacenará los nombres de los amigos ingresados.
+ */
+let nombreAmigos = [];
