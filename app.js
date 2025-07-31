795384c0c2fe0b28bef09812f7d4cf5aee0abb9b
yamilrafart2 <yamilrafart2>
2025-07-31 20:50:13 -0300

    Implementa la función actualizarLista

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ function agregarAmigo() {
     } else {
         nombreAmigos.push(document.getElementById('amigo').value);
         document.getElementById('amigo').value = '';
+        actualizarLista();
     }
+    return;
+}
+
+/*
+    Función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML.
+    *   Tareas específicas:
+        -   Obtiene el elemento de la lista.
+        -   Limpia la lista existente para asegurarse de que no haya duplicados al actualizar.
+        -   Itera sobre el arreglo, recorre el arreglo amigos y crea elementos de lista (<li>) para cada título.
+        -   Agrega elementos a la lista: Para cada amigo, crea un nuevo elemento de lista.
+*/
+function actualizarLista() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    nombreAmigos.forEach(unNombre => {
+        let item = document.createElement('li');
+        item.textContent = unNombre;
+        listaAmigos.appendChild(item);
+    });
     return;
 }
